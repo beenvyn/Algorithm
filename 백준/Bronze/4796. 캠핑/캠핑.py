849bc49c12042fe0115f3853444142ea2f881c98
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2025-03-25 11:37:42 +0900

    [Bronze I] Title: 캠핑, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4796. 캠핑/캠핑.py
@@ -0,0 +1,10 @@
+i = 1
+
+while True:
+    l, p, v = map(int, input().split())
+    if l == 0 and p == 0 and v == 0:
+        break
+    
+    answer = (v // p) * l + min((v % p), l)
+    print(f'Case {i}: {answer}')
+    i += 1
