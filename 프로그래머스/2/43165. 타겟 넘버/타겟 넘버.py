709ea8ccf10f5c6908b01313237eb18dac600846
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2025-05-03 15:03:15 +0900

    [level 2] Title: 타겟 넘버, Time: 415.29 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,15 @@
+def solution(numbers, target):
+    answer = 0
+    n = len(numbers)
+    
+    def dfs(idx, result):
+        nonlocal answer
+        if idx == n:
+            if result == target:
+                answer += 1
+        else:
+            dfs(idx+1, result+numbers[idx])
+            dfs(idx+1, result-numbers[idx])
+    
+    dfs(0, 0)
+    return answer
