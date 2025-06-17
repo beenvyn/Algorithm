e1a52fa7f15282cba57c82af62eafebe68852195
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2025-06-17 16:22:04 +0900

    [level 2] Title: 타겟 넘버, Time: 336.63 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,17 @@
 def solution(numbers, target):
-    answer = 0
     n = len(numbers)
+    answer = 0
     
     def dfs(idx, result):
         nonlocal answer
+        
         if idx == n:
             if result == target:
                 answer += 1
+                return
         else:
             dfs(idx+1, result+numbers[idx])
             dfs(idx+1, result-numbers[idx])
-    
-    dfs(0, 0)
+            
+    dfs(0,0)
     return answer
