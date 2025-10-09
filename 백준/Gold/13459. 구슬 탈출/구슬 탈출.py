46fd9ca458569f332adade54c0344d1ada01179a
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2025-10-09 17:22:30 +0900

    [Gold I] Title: 구슬 탈출, Time: 64 ms, Memory: 35036 KB -BaekjoonHub

--- 백준/Gold/13459. 구슬 탈출/구슬 탈출.py
+++ 백준/Gold/13459. 구슬 탈출/구슬 탈출.py
@@ -1,80 +1,67 @@
-from collections import deque
 import sys
-
+from collections import deque
+# sys.setrecursionlimit(300000)
 input = sys.stdin.readline
 
-row, col = map(int, input().split())
-grid = []
-for _ in range(row):
-    grid.append(input().rstrip())
+rows, cols = map(int, input().split())
+graph = [input() for _ in range(rows)]
 
-def bfs(row,col,grid):
-    dx = [0,0,-1,1]
-    dy = [-1,1,0,0]
+for r in range(rows):
+    for c in range(cols):
+        if graph[r][c] == 'R':
+            rsr, rsc = r, c
+        elif graph[r][c] == 'B':
+            bsr, bsc = r, c
 
-    # 다음 위치 계산
-    def move(x,y,dir):
-        nx, ny = x, y
-        while True:
-            nx = nx + dx[dir]
-            ny = ny + dy[dir]
+visited = set()
+dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+que = deque([(rsr,rsc,bsr,bsc,0)])
+visited.add((rsr,rsc,bsr,bsc))
 
-            if grid[nx][ny] == 'O':
-                break
+def move(r,c,dir):
+    nr, nc = r, c
+    while True:
+        nr += dirs[dir][0]
+        nc += dirs[dir][1]
 
-            if grid[nx][ny] == '#':
-                nx -= dx[dir]
-                ny -= dy[dir]
-                break
+        if graph[nr][nc] == '#':
+            nr -= dirs[dir][0]
+            nc -= dirs[dir][1]
+            break
+        elif graph[nr][nc] == 'O':
+            break
 
-        return (nx, ny)
+    return (nr, nc)
+
+
+while que:
+    rr, rc, br, bc, cnt = que.popleft()
+
+    if cnt >= 10:
+        continue
     
-    # red, blue 시작 위치 찾기
-    for x in range(row):
-        for y in range(col): 
-            if grid[x][y] == 'R':
-                rx, ry = x, y
-            if grid[x][y] == 'B':
-                bx, by = x, y
+    for dir in range(4):
+        nrr, nrc = move(rr,rc,dir)
+        nbr, nbc = move(br,bc,dir)
 
-    # queue에 시작점 예약 (red좌표, blue좌표, cnt)
-    visited = set()
-    q = deque([(rx,ry,bx,by,0)])
-    visited.add((rx,ry,bx,by,0))
+        if graph[nbr][nbc] == 'O':
+            continue
 
-    while q:
-        # 현재 위치 방문
-        rx, ry, bx, by, cnt = q.popleft()
-
-        if cnt > 10:
-            return 0
-
-        # 빨간 구슬이 구멍에 빠진 경우
-        if grid[rx][ry] == 'O':
-            return 1
-
-        for dir in range(4):
-            nrx, nry = move(rx,ry,dir)
-            nbx, nby = move(bx,by,dir)
-
-            if grid[nbx][nby] == 'O':
-                continue
-            
-            # 만약 red, blue 위치가 같으면 조정
-            if nrx == nbx and nry == nby:
-                # 빨간 구슬이 더 멀리 있는 경우
-                if abs(nrx - rx) + abs(nry - ry) > abs(nbx - bx) + abs(nby - by):
-                    nrx -= dx[dir]
-                    nry -= dy[dir]
-                else: # 파란 구슬이 더 멀리 있는 경우
-                    nbx -= dx[dir]
-                    nby -= dy[dir]
+        if graph[nrr][nrc] == 'O':
+            print(1)
+            sys.exit(0)
         
-            if (nrx, nry, nbx, nby) not in visited:
-                q.append((nrx, nry, nbx, nby, cnt + 1))
-                visited.add((nrx, nry, nbx, nby))
-    return 0
+        if nrr == nbr and nrc == nbc:
+            if abs(nrr - rr) + abs(nrc - rc) < abs(nbr - br) + abs(nbc - bc):
+                nbr -= dirs[dir][0]
+                nbc -= dirs[dir][1]
+            else:
+                nrr -= dirs[dir][0]
+                nrc -= dirs[dir][1]
 
-print(bfs(row,col,grid))
+        if (nrr,nrc,nbr,nbc) not in visited:        
+            que.append((nrr,nrc,nbr,nbc,cnt + 1))
+            visited.add((nrr,nrc,nbr,nbc))
 
+print(0)
         
