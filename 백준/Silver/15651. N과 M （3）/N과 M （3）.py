5803ea877445f5e27308ca42c6c272379646c976
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2025-10-14 13:26:28 +0900

    [Silver III] Title: N과 M (3), Time: 1948 ms, Memory: 143376 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+answer = []
+
+def backtrack(curr):
+    if len(curr) == m:
+        answer.append(curr[:])
+        return
+
+    for num in range(1,n+1):
+        curr.append(num)
+        backtrack(curr)
+        curr.pop()
+
+backtrack([])
+for x in answer:
+    print(' '.join(map(str,x)))
