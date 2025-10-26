cbc6fa72c516f7997b757d5fc0e68cf4efe8de2f
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2025-10-26 21:06:51 +0900

    [Gold IV] Title: 강의실 배정, Time: 348 ms, Memory: 75768 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,19 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+N = int(input())
+times = [list(map(int,input().split())) for _ in range(N)]
+times.sort()
+
+heap = []
+answer = 0
+for start, end in times:
+    if heap and heap[0] <= start:
+        heapq.heappop(heap)
+    
+    heapq.heappush(heap,end)
+    answer = max(answer,len(heap))
+
+print(answer)
