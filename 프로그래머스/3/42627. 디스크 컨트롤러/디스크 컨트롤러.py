efc25e3930c6ff2c4ff97020ed6c156800a23f21
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2025-11-01 00:58:56 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 10.87 ms, Memory: 9.28 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -0,0 +1,24 @@
+import heapq
+
+def solution(jobs):
+    answer = 0
+    now = 0 # 현재 시간
+    i = 0 # 처리 개수
+    start = -1 # 마지막 완료 시간
+    heap = []
+    
+    while i < len(jobs):
+        for job in jobs:
+            if start < job[0] <= now:
+                heapq.heappush(heap,(job[1],job[0]))
+        
+        if heap:
+            x = heapq.heappop(heap)
+            start = now
+            now += x[0]
+            answer += now - x[1]
+            i += 1
+        else:
+            now += 1
+    
+    return answer // len(jobs)
