bea2bd35860d8796fb5bdc78564d8e7d3871a423
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-01-05 16:36:36 +0900

    [Silver I] Title: 안전 영역, Time: 992 ms, Memory: 34712 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -2,32 +2,31 @@ import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-n = int(input())
-grid = [list(map(int, input().split())) for _ in range(n)]
+N = int(input())
+grid = [list(map(int, input().split())) for _ in range(N)]
 
+max_h = max(map(max, grid))
 dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-max_height = max(map(max, grid))
 answer = 1
 
-def dfs(r,c):
+def dfs(r, c):
     visited[r][c] = True
 
     for d in dirs:
         nr, nc = r + d[0], c + d[1]
 
-        if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] > h and not visited[nr][nc]:
+        if 0 <= nr < N and 0 <= nc < N and not visited[nr][nc] and grid[nr][nc] > h:
             dfs(nr, nc)
 
-for h in range(1, max_height + 1):
-    visited = [[False] * n for _ in range(n)]
+for h in range(1, max_h + 1):
+    visited = [[False] * N for _ in range(N)]
     cnt = 0
 
-    for r in range(n):
-        for c in range(n):
+    for r in range(N):
+        for c in range(N):
             if grid[r][c] > h and not visited[r][c]:
                 dfs(r, c)
                 cnt += 1
     answer = max(answer, cnt)
-    
-print(answer)
 
+print(answer)   
