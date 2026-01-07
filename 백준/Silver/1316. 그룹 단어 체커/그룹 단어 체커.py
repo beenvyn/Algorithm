147cf600ede82f2c70202635865ec5d24f820ff3
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-01-07 10:55:33 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,18 +1,18 @@
-from collections import Counter
+import sys
+input = sys.stdin.readline
 
-n = int(input())
+N = int(input())
+words = [input().rstrip() for _ in range(N)]
 answer = 0
 
-for i in range(n):
-    word = input()
-    word_count = Counter(word)
-
-    for char, cnt in word_count.items():
-        if cnt > 1:
-            if char * cnt not in word:
+for word in words:
+    chars = [word[0]]
+    for i in range(1, len(word)):
+        if word[i-1] != word[i]:
+            if word[i] in chars: # 이전에 이미 등장한 문자면
                 break
-    
-    else:
+            else:
+                chars.append(word[i])
+    else: # 중간에 break 없이 단어를 다 돈 경우
         answer += 1
-      
 print(answer)
