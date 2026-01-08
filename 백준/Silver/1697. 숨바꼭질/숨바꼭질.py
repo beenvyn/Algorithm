e3292c74e119cb245427f0d9c8bde4882151c0e1
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-01-08 10:40:20 +0900

    [Silver I] Title: 숨바꼭질, Time: 200 ms, Memory: 39220 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,20 +1,21 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
 start, end = map(int, input().split())
+
+que = deque([(start, 0)])
 max_range = max(start, end) * 2 + 1
 visited = [False] * max_range
 
-q = deque([(start, 0)])
-visited[start] = True
+while que:
+    cur, time = que.popleft()
 
-while q:
-    cur_x, cnt = q.popleft()
-
-    if cur_x == end:
-        print(cnt)
+    if cur == end:
+        print(time)
         break
 
-    for c in [cur_x - 1, cur_x + 1, cur_x * 2]:
-        if 0 <= c < max_range and not visited[c]:
-            q.append((c, cnt + 1))
-            visited[c] = True
+    for x in [cur + 1, cur - 1, cur * 2]:
+        if 0 <= x < max_range and not visited[x]:
+            visited[x] = True
+            que.append((x, time + 1))
