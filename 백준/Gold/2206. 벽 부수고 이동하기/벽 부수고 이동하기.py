32c0d4372ddbb3ca3b17e59c48a9346f3cc2fe69
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-01-09 11:19:44 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 4912 ms, Memory: 151436 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -3,31 +3,29 @@ from collections import deque
 input = sys.stdin.readline
 
 rows, cols = map(int, input().split())
-graph = [input().rstrip() for _ in range(rows)]
+grid = [list(input().rstrip()) for _ in range(rows)]
 
-dirs = [(0,1),(0,-1),(1,0),(-1,0)]
-visited = [[[0,0] for _ in range(cols)] for _ in range(rows)]
-
-que = deque([(0,0,1,1)])
+dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+visited = [[[0, 0] for _ in range(cols)] for _ in range(rows)]
+que = deque([(0, 0, 1, 1)])
 visited[0][0][0] = 1
 
 while que:
     r, c, dist, flag = que.popleft()
-
     if r == rows - 1 and c == cols - 1:
         print(dist)
-        sys.exit(0)
+        break
 
     for d in dirs:
         nr, nc = r + d[0], c + d[1]
 
         if 0 <= nr < rows and 0 <= nc < cols and not visited[nr][nc][flag]:
-            if flag and graph[nr][nc] == "1":
-                que.append((nr,nc,dist+1,0))
-                visited[nr][nc][0] = 1
-            
-            if graph[nr][nc] == "0":
-                que.append((nr,nc,dist+1,flag))
+            if grid[nr][nc] == '0': # 일반 이동
+                que.append((nr, nc, dist + 1, flag))
                 visited[nr][nc][flag] = 1
         
-print(-1)
+            if flag and grid[nr][nc] == '1': # 벽 부수기
+                que.append((nr, nc, dist + 1, 0))
+                visited[nr][nc][0] = 1 # 벽을 부순 후에는 flag=0이 됨.
+else:
+    print(-1)
