ab28ccc0e70b2dfbc20520539a099a359ceb0973
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-01-26 15:47:44 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.91 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,28 +1,26 @@
 import heapq
 
 def solution(jobs):
-    jobs.sort() # 요청 시각 오름차순 정렬
     n = len(jobs)
+    jobs.sort()
     
-    idx = 0 # 아직 힙에 안넣은 jobs의 인덱스
-    t = 0 # 현재 시각
-    heap = [] # (소요시간, 요청시간)
-    done = 0 # 처리한 작업 개수
-    answer = 0
+    idx = 0 # 아직 힙에 안넣은 job의 인덱스
+    pq = [] # 대기큐
+    cur_time = 0 # 현재 시각
+    cnt = 0 # 처리한 작업 수
+    total = 0 # 반환 시간의 합
     
-    while done < n:
-        # 현재 시각 t까지 도착한 작업을 전부 힙에 넣기
-        while idx < n and jobs[idx][0] <= t:
-            heapq.heappush(heap, (jobs[idx][1], jobs[idx][0]))
+    while cnt < n:
+        while idx < n and jobs[idx][0] <= cur_time:
+            heapq.heappush(pq, (jobs[idx][1], jobs[idx][0]))
             idx += 1
         
-        if heap:
-            current = heapq.heappop(heap)
-            t += current[0]
-            answer += (t - current[1])
-            done += 1
+        if pq:
+            l, s = heapq.heappop(pq)
+            cur_time += l
+            total += (cur_time - s)
+            cnt += 1
         else:
-            # 아직 도착한 작업이 없다면, 다음 작업의 요청시각으로 점프
-            t = jobs[idx][0]
-    
-    return answer // n
+            cur_time = jobs[idx][0]
+            
+    return total // n
