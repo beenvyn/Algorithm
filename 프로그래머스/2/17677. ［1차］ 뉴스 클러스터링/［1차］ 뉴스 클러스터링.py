1121034e83f144d6d2b26f4e3515f591e087e0a0
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-02-19 15:10:15 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.61 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -3,26 +3,26 @@ from collections import Counter
 def solution(str1, str2):
     answer = 0
     
-    # 다중집합 만들기
-    def multi_set(s):
-        s = s.upper()
-        res = []
-        for i in range(len(s) -1):
-            word = s[i:i+2]
-            if word.isalpha():
-                res.append(word)
-        return res
+    def get_multi_set(s):
+        multi_set = []
+        s = s.lower()
         
-    A = Counter(multi_set(str1))
-    B = Counter(multi_set(str2))
+        for i in range(len(s) - 1):
+            x = s[i:i+2]
+            if x.isalpha():
+                multi_set.append(x)
+        return multi_set
     
-    inter = A & B # 같은 원소가 있으면 "최소 개수"만 남김
-    union = A | B # 같은 원소 있으면 "최대 개수" 유지
+    str1_counter = Counter(get_multi_set(str1))
+    str2_counter = Counter(get_multi_set(str2))
+    
+    inter = str1_counter & str2_counter
+    union = str1_counter | str2_counter
     
     inter_size = sum(inter.values())
     union_size = sum(union.values())
-        
-    if union_size == 0: # 2글자 알파벳 조합이 하나도 없을 때
+    
+    if union_size == 0:
         return 65536
     
-    return int(inter_size / union_size * 65536)
+    return int((inter_size / union_size) * 65536)
