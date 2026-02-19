c07a2e00e1ded345908aae22a82295bf55e3bc73
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-02-19 22:27:23 +0900

    [level 2] Title: 미로 탈출, Time: 11.85 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,41 +1,36 @@
 from collections import deque
 
-def bfs(start, end, maps):
-    col = len(maps)
-    row = len(maps[0])
-    
-    dx = [0,0,-1,1]
-    dy = [-1,1,0,0]
-    distance = [[0] * row for _ in range(col)]
-    
-    for i in range(col):
-        for j in range(row):
-            if maps[i][j] == start:
-                sy, sx = i, j
-            elif maps[i][j] == end:
-                ey, ex = i, j
-                
-    queue = deque([(sy, sx)])
-    
-    while queue:
-        y, x = queue.popleft()
-                
-        for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
-        
-            if 0 <= nx < row and 0 <= ny < col and not distance[ny][nx] and maps[ny][nx] != 'X':
-                if nx == ex and ny == ey:
-                    return distance[y][x] + 1
-                distance[ny][nx] = distance[y][x] + 1
-                queue.append((ny, nx))
-    return -1
-
 def solution(maps):
-    to_lever = bfs('S', 'L', maps)
-    to_end = bfs('L', 'E', maps)
+    dirs = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+    rows, cols = len(maps), len(maps[0])
     
-    if to_lever != -1 and to_end != -1:
-        return to_lever + to_end
-    else:
+    def bfs(start, end):
+        visited = [[False] * cols for _ in range(rows)]
+        
+        for r in range(rows):
+            for c in range(cols):
+                if maps[r][c] == start:
+                    que = deque([(r, c, 0)])
+                    visited[r][c] = True
+                    
+        while que:
+            r, c, time = que.popleft()
+            
+            if maps[r][c] == end:
+                return time
+            
+            for d in dirs:
+                nr, nc = r + d[0], c + d[1]
+                
+                if 0 <= nr < rows and 0 <= nc < cols and maps[nr][nc] != 'X' and not visited[nr][nc]:
+                    visited[nr][nc] = True
+                    que.append((nr, nc, time + 1))
         return -1
+    
+    to_lever = bfs('S', 'L')
+    to_exit = bfs('L', 'E')
+    
+    if to_lever == -1 or to_exit == -1:
+        return -1
+    else:
+        return to_lever + to_exit
