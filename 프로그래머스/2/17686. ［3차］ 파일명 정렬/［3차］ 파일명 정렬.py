eaddf73e180f3bd10d7209b85b1edce46592e94c
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-02-19 15:25:05 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 5.44 ms, Memory: 9.71 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,26 +1,24 @@
 def solution(files):
     answer = []
-
+    # head, number 분리
     def parse(file):
-        # 숫자 시작 위치 찾기
-        start = 0
-        while start < len(file) and not file[start].isdigit():
-            start += 1
+        num_start = 0
+        while num_start < len(file) and not file[num_start].isdigit():
+            num_start += 1
         
-        # 숫자 최대 5자리 추출
-        end = start
-        while end < len(file) and file[end].isdigit() and end - start < 5:
-            end += 1
+        num_end = num_start
+        while num_end < len(file) and file[num_end].isdigit() and num_end - num_start < 5:
+            num_end += 1
         
-        head = file[:start]
-        number = file[start:end]
+        head = file[:num_start]
+        number = file[num_start:num_end]
         return head, number
     
     parsed_files = []
     for file in files:
         head, number = parse(file)
-        parsed_files.append((head.lower(), int(number), file)) # 원본 저장
+        parsed_files.append((head.lower(), int(number), file))
     
     parsed_files.sort(key=lambda x:(x[0], x[1]))
     
-    return [ x[2] for x in parsed_files ]
+    return [f[2] for f in parsed_files]
