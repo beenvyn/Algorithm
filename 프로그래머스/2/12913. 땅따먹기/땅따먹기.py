36594afa9318e48236d4aa3d78c82e3c540c35f7
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-02-22 12:43:22 +0900

    [level 2] Title: 땅따먹기, Time: 201.29 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,10 +1,6 @@
 def solution(land):
-    answer = 0
-    n = len(land)
-    dp = [row[:] for row in land] # dp[r][c]: r행 c열까지 왔을 때 얻을 수 있는 최대 점수
-    
     for r in range(1, len(land)):
-        for c in range(4): # 바로 위 행(r-1)에서 같은 열(c)을 제외한 열들 중 최대값을 더함
-            dp[r][c] += max(dp[r-1][k] for k in range(4) if k != c)
+        for c in range(4):
+            land[r][c] += max(land[r-1][:c] + land[r-1][c+1:])
 
-    return max(dp[-1])
+    return max(land[-1])
