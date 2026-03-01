8cbc8d1f331d9883a794a6bcd48b7f79d7f822be
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-03-01 15:55:06 +0900

    [level 2] Title: 테이블 해시 함수, Time: 82.75 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -1,15 +1,9 @@
 def solution(data, col, row_begin, row_end):
-    sorted_data = sorted(data, key=lambda x:(x[col - 1], -x[0]))
+    data.sort(key=lambda x:(x[col - 1], -x[0]))
     
-    S = []
+    answer = 0
     for i in range(row_begin, row_end + 1):
-        temp = 0
-        for d in sorted_data[i-1]:
-            temp += d % i
-        S.append(temp)
-    
-    answer = S[0]
-    for s in S[1:]:
-        answer = answer ^ s
+        s_i = sum(v % i for v in data[i-1])
+        answer ^= s_i
         
     return answer
