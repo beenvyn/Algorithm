27ac5d2be0839f006d441eaffb719eaaf9be600d
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-03-05 16:47:34 +0900

    [Silver II] Title: 나무 자르기, Time: 2752 ms, Memory: 148224 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,27 +1,25 @@
 import sys
-
 input = sys.stdin.readline
 
-N, M = map(int,input().split())
-heights = list(map(int,input().split()))
+N, M = map(int, input().split())
+heights = list(map(int, input().split()))
 
-left, right = 0, max(heights)
 answer = 0
+l, r = 0, max(heights)
 
-while left <= right:
-    mid = (left + right) // 2
-
+while l <= r:
+    mid = (l + r) // 2 # 절단 높이
     total = 0
     for h in heights:
         if h > mid:
             total += h - mid
-            if total >= M:    # 목표량 채우면 더 안 더해도 됨
+            if total >= M: # 목표량 채우면 더 안해도 됨 
                 break
     
     if total >= M:
         answer = mid
-        left = mid + 1
+        l = mid + 1
     else:
-        right = mid - 1
-
+        r = mid - 1
+    
 print(answer)
