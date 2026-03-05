b89ef76acb0e9aa7dbb2aa1a3cd56962d4f1191f
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-03-05 17:08:01 +0900

    [level 3] Title: 입국심사, Time: 599.25 ms, Memory: 13.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,21 +1,18 @@
 def solution(n, times):
     answer = 0
-    left = min(times)
-    right = max(times) * n
+    l, r = 0, max(times) * n # 시간(정답) 후보
     
-    while left <= right:
-        checked = 0
-        mid = (left + right) // 2
-        for time in times:
-            checked += mid // time
+    # T분 안에 n명 이상 심사할 수 있나?
+    while l <= r:
+        mid = (l + r) // 2
         
-            if checked >= n:
-                break
-    
-        if checked >= n:
+        cnt = 0
+        for t in times:
+            cnt += mid // t
+        
+        if cnt >= n:
             answer = mid
-            right = mid - 1
+            r = mid - 1
         else:
-            left = mid + 1
-
+            l = mid + 1
     return answer
