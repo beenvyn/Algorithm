54fd1ff94df78bc6ff55195572af18c7ef72e5e7
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-03-20 16:57:04 +0900

    [level 2] Title: 광물 캐기, Time: 0.05 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -1,37 +1,29 @@
 def solution(picks, minerals):
     answer = 0
+    pick_idx = 0
+    fatigue_dict = {'diamond': [1, 5, 25], 'iron': [1, 1, 5], 'stone': [1, 1, 1]}
+    max_minerals = sum(picks) * 5
+
+    groups = [minerals[i:i+5] for i in range(0, max_minerals, 5)] # 5개씩 묶음
+    groups_cnt = [[idx, 0, 0, 0] for idx in range(len(groups))] # 묶음별 인덱스, 다이아 곡갱이 수, 철 곡갱이 수, 돌 곡갱이 수
     
-    total_picks = sum(picks) * 5
-    if len(minerals) > total_picks: #  곡괭이 보다 광물이 많은 경우
-        minerals = minerals[:total_picks] # 곡괭이로 캘 수 있는 만큼만 자른다
+    for i in range(len(groups)):
+        for m in groups[i]:
+            if m == 'diamond':
+                groups_cnt[i][1] += 1
+            elif m == 'iron':
+                groups_cnt[i][2] += 1
+            else:
+                groups_cnt[i][3] += 1
     
-    # 광물 5개씩 묶어서 다이아, 철, 돌 갯수 세기
-    mineral_groups = [[0,0,0] for _ in range(len(minerals) // 5 + 1)]
-    for i in range(len(minerals)):
-        if minerals[i] == 'diamond':
-            mineral_groups[i//5][0] += 1
-        elif minerals[i] == 'iron':
-            mineral_groups[i//5][1] += 1
-        elif minerals[i] == 'stone':
-            mineral_groups[i//5][2] += 1
+    groups_cnt.sort(key=lambda x:(-x[1], -x[2], -x[3])) # 난이도 어려운 순으로 정렬
     
-    mineral_groups.sort(key=lambda x:(-x[0], -x[1], -x[2])) # 난이도가 어려운 순으로 정렬
-    
-    # 정렬된 광물들을 다이아, 철, 돌 곡괭이 순서대로 캔다.
-    for group in mineral_groups:
-        dia, iron, stone = group
-        for j in range(len(picks)):
-            if picks[j] > 0 and j == 0: # 다이아 곡괭이
-                answer += dia + iron + stone
-                picks[j] -= 1
-                break
-            elif picks[j] > 0 and j == 1: # 철 곡괭이
-                answer += (dia * 5) + iron + stone
-                picks[j] -= 1
-                break
-            elif picks[j] > 0 and j == 2: # 돌 곡괭이
-                answer += (dia * 25) + (iron * 5) + stone
-                picks[j] -= 1
-                break
-    
+    for i in range(len(groups_cnt)):
+        cur_minerals = groups[groups_cnt[i][0]]
+        while pick_idx < 3 and picks[pick_idx] == 0:
+            pick_idx += 1
+        for m in cur_minerals:
+            answer += fatigue_dict[m][pick_idx]
+        picks[pick_idx] -= 1
+
     return answer
