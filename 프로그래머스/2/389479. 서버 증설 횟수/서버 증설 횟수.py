4f202f86756f29ebd4e70a5992c981a1d549bead
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-03-20 15:05:14 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.36 ms, Memory: 8.94 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
@@ -1,17 +1,18 @@
-from collections import deque
+import heapq
 
 def solution(players, m, k):
     answer = 0
-    que = deque([]) # 서버 만료 시간
+    heap = [] # 현재 돌아가고 있는 서버의 증설 시간
     
-    for time, player in enumerate(players):
-        while que and que[0] == time:
-            que.popleft()
+    for t in range(len(players)):
+        n = players[t] // m # 필요한 서버 수
         
-        needed_server, current_server = player // m, len(que)
-        if needed_server > current_server:
-            for _ in range(needed_server - current_server):
-                que.append(time + k)
+        while heap and heap[0] + k == t: # 만료된 서버 없애기
+            heapq.heappop(heap)
+        
+        if len(heap) < n: # 현재 돌아가고 있는 서버 수가 필요한 서버 수 보다 적으면
+            for _ in range(n - len(heap)): # 필요한 만큼 서버 새로 증설
+                heapq.heappush(heap, t)
                 answer += 1
-            
+    
     return answer
