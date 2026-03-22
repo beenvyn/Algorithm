831c11767470dcdbd1ebfdee6b6370aa906506b8
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-03-22 16:16:50 +0900

    [level 2] Title: 디펜스 게임, Time: 406.88 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
@@ -1,15 +1,22 @@
 import heapq
 
 def solution(n, k, enemy):
-    heap = []
+    # 적 수를 계속 저장
+    # 병사 먼저 깎는다
+    # 병사가 부족하면 지금까지 중 가장 큰 적을 무적권으로 되돌리기
+
+    heap = [] # 물리친 적 수 저장(최대 힙)
+    
     for i, e in enumerate(enemy):
-        heapq.heappush(heap,-e)
+        heapq.heappush(heap, -e) # 일단 병사로 막음
         n -= e
         
-        if n < 0:
+        if n < 0: # 병사가 부족하면 무적권으로 되돌리기
             if k > 0:
                 n += -heapq.heappop(heap)
                 k -= 1
             else:
                 return i
+            
+    # 전부 물리친 경우
     return len(enemy)
