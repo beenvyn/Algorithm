f0e621ea95946aab23d62734a57c76d466dc56d0
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-03-22 14:29:36 +0900

    [level 2] Title: 숫자 변환하기, Time: 303.65 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,18 +1,20 @@
 from collections import deque
 
 def solution(x, y, n):
-    graph = [0] * (y + 1)
+    answer = 0
+    visited = [False] * (y + 1)
+
+    que = deque([(x, 0)])
     
-    queue = deque([x])
-    
-    while queue:
-        cur_val = queue.popleft()
+    while que:
+        cur_x, cnt = que.popleft()
         
-        if cur_val == y:
-            return graph[cur_val]
-        for num in [cur_val + n, cur_val * 2, cur_val * 3]:
-            if num <= y and not graph[num]:
-                graph[num] = graph[cur_val] + 1
-                queue.append(num)
-                
+        if cur_x == y:
+            return cnt
+        
+        for nxt in [cur_x + n, cur_x * 2, cur_x * 3]:
+            if nxt <= y and not visited[nxt]:
+                visited[nxt] = True
+                que.append((nxt, cnt + 1))
+    
     return -1
