ded2c91627d2235a7bbd81ba35979784733f5bd4
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-03-22 15:11:17 +0900

    [level 2] Title: 시소 짝꿍, Time: 63.04 ms, Memory: 13.1 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -1,21 +1,36 @@
-from collections import Counter
+import math
 
 def solution(weights):
     answer = 0
-    counter = Counter(weights)
+    weights.sort()
     
-    for key, val in counter.items():
-        if val >= 2:
-            answer += val * (val - 1) // 2
+    # 현재 몸무게와 짝이 되는 이전 몸무게가 몇 개 있었는지 세기
+    # cnt_dict[몸무게] = 지금까지 나온 개수
+    cnt_dict = {} 
     
-    weights = set(weights)
+    # 가능한 비율 후보: 1:1, 2:3, 1:2(2:4), 3:4
     
     for w in weights:
-        if w*2/3 in weights:
-            answer += counter[w] * counter[w*2/3]
-        if w*2/4 in weights:
-            answer += counter[w] * counter[w*2/4]
-        if w*3/4 in weights:
-            answer += counter[w] * counter[w*3/4]
+        if w in cnt_dict:
+            answer += cnt_dict[w]
         
+        if (w * 2) % 3 == 0:
+            target = (w * 2) // 3
+            if target in cnt_dict:
+                answer += cnt_dict[target]
+        
+        if w % 2 == 0:
+            target = w // 2
+            if target in cnt_dict:
+                answer += cnt_dict[target]
+        
+        if (w * 3) % 4 == 0:
+            target = (w * 3) // 4
+            if target in cnt_dict:
+                answer += cnt_dict[target]
+        
+        if w not in cnt_dict:
+            cnt_dict[w] = 0
+        cnt_dict[w] += 1    
+    
     return answer
