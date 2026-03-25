24436d212bf1c48b560902271a0e9915a4880e3e
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-03-25 16:07:02 +0900

    [level 2] Title: [1차] 캐시, Time: 87.29 ms, Memory: 23.6 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,30 +1,23 @@
 from collections import deque
 
 def solution(cacheSize, cities):
+    answer = 0
+    que = deque([])
+    cities = [c.lower() for c in cities]
+    
     if cacheSize == 0:
         return len(cities) * 5
     
-    answer = 0
-    que = deque([]) 
-    
+    # 가장 오래된게 왼쪽, 가장 최신게 오른쪽
     for city in cities:
-        lower_city = city.lower()
-        if len(que) < cacheSize: # 캐시가 덜 찬 경우
-            if lower_city not in que: # miss
-                que.append(lower_city)
-                answer += 5
-            else: # hit
-                que.remove(lower_city)
-                que.append(lower_city)
-                answer += 1
-        else:  # 캐시가 다 찬 경우
-            if lower_city not in que: # miss
+        if city in que: # hit
+            que.remove(city) # 기존 위치에서 제거
+            que.append(city) # 가장 오른쪽으로 위치 옮김
+            answer += 1
+        else: # miss
+            if len(que) >= cacheSize:
                 que.popleft()
-                que.append(lower_city)
-                answer += 5
-            else: # hit
-                que.remove(lower_city)
-                que.append(lower_city)
-                answer += 1
+            que.append(city)
+            answer += 5
             
     return answer
