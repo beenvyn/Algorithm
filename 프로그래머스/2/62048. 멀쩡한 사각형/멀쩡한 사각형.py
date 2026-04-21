3ece525589056a014d8b77e3012799a45d342a08
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2026-04-21 12:40:34 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
@@ -1,8 +1,7 @@
 import math
 
 def solution(w,h):
-    # 각 블록마다 잘리는 칸 수: 가로 + 세로 - 1 (a + b - 1)
-    # a는 w를 최소 공약수로 나눈 값, b는 h를 최소 공약수로 나눈 값이고 이게 최소 공약수 번 반복되므로 
-    # (w/g + h/g - 1) * g
-    unusable = w + h - math.gcd(w, h)
-    return w * h - unusable
+    g = math.gcd(w, h)
+    unusable = w // g + h // g - 1
+
+    return w * h - (unusable * g)
